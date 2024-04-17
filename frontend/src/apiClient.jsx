67206046f515f4b15feddce4c3ecdb2a4d8b9433
Geetam <geetambinani6@gmail.com>
2024-04-17 18:49:48 +0530

    initial  commit

--- frontend/src/apiClient.jsx
+++ frontend/src/apiClient.jsx
@@ -1,6 +1,8 @@
-const API_BASE_URL = "http://localhost:7000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+
 
 export const getFeatures = async (page) => {
+  console.log(API_BASE_URL);
   const response = await fetch(`${API_BASE_URL}/api/v1/get-scroll-feature?page=${page}`);
   if (!response.ok) {
     throw new Error("Failed to fetch features");
